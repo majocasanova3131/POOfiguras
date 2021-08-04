b6a46182727d722bff549e8afe7aaacd0741865d
majocasanova3131 <mcasanova@edu.upolitecnica.cr>
2021-08-03 21:26:44 -0500

    Se agrega el mensaje en el main

--- src/poofiguras/figuras.java
+++ src/poofiguras/figuras.java
@@ -1,6 +1,6 @@
 package poofiguras;
 import javax.swing.JOptionPane;
-public class figuras {
+public class Figuras {
 public void cuadrado(){
     int lado,area;
     lado=Integer.parseInt(JOptionPane.showInputDialog("Digite el lado del cuadrado"));
@@ -13,6 +13,20 @@ public void triangulo(){
     base=Integer.parseInt(JOptionPane.showInputDialog("Digite la base del triangulo"));
     altura=Integer.parseInt(JOptionPane.showInputDialog("Digite la altura del triangulo"));
     area=(base*altura/2);
+    JOptionPane.showMessageDialog(null,"El area del triangulo es"+area);
+}
+public void Rectangulo(){
+    int largo,ancho,area;
+    largo=Integer.parseInt(JOptionPane.showInputDialog("Digite el largo del rectangulo"));
+    ancho=Integer.parseInt(JOptionPane.showInputDialog("Digite el ancho  del rectangulo"));
+    area=(largo*ancho);
+    JOptionPane.showMessageDialog(null,"El area del triangulo es"+area);
+}
+public void Pentagono(){
+    int perimetro,altura,area;
+    perimetro=Integer.parseInt(JOptionPane.showInputDialog("Digite el perimetro del pentagono"));
+    altura=Integer.parseInt(JOptionPane.showInputDialog("Digite la altura  del pentagono"));
+    area=(perimetro*altura/2);
     JOptionPane.showMessageDialog(null,"El area del triangulo es"+area);
 }
 
