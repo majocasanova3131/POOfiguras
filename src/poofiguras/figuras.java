1c35f19ee24d17cb2bac3612bf5c119d08f8ee5f
majocasanova3131 <mcasanova@edu.upolitecnica.cr>
2021-08-03 20:46:25 -0500

    Se agrega el Proyecto

---
+++ src/poofiguras/figuras.java
@@ -0,0 +1,19 @@
+package poofiguras;
+import javax.swing.JOptionPane;
+public class figuras {
+public void cuadrado(){
+    int lado,area;
+    lado=Integer.parseInt(JOptionPane.showInputDialog("Digite el lado del cuadrado"));
+    area=lado*lado;
+    JOptionPane.showMessageDialog(null,"El area del circulo es."+area);
+}
+
+public void triangulo(){
+    int base,altura,area;
+    base=Integer.parseInt(JOptionPane.showInputDialog("Digite la base del triangulo"));
+    altura=Integer.parseInt(JOptionPane.showInputDialog("Digite la altura del triangulo"));
+    area=(base*altura/2);
+    JOptionPane.showMessageDialog(null,"El area del triangulo es"+area);
+}
+
+}
